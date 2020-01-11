2d02011c7acb3d2d724169197157e3f5fb5dc278
savicci <koziol.grzesiek@interia.pl>
2020-01-11 16:05:15 +0100

    Adding basic navbar.

--- src/components/navbar/NavigationBar.js
+++ src/components/navbar/NavigationBar.js
@@ -1,9 +1,48 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-export class NavigationBar extends React.Component{
-    render(){
+import "./Navbar.css";
+
+export class NavigationBar extends React.Component {
+    render() {
         return (
-            <h1>Navigation Bar</h1>
+                <Navbar bg="light" expand="sm">
+                    <Navbar.Brand href="/">
+                        <img src={require("../../images/favicon.ico")}
+                             width="40"
+                             height="40"
+                             className="d-inline-block align-top"
+                             alt="UsterkaLogo"
+                        />
+                        Usterka
+                    </Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link className='usterka-option' href="/">Strona główna</Nav.Link>
+                            <Nav.Link className='usterka-option' href="/search">Wyszukiwarka</Nav.Link>
+                        </Nav>
+
+                        <Nav>
+                            <img src={require("../../images/honk.jpg")}
+                                 alt="Pepe"
+                                 width="60"
+                                 height="50"
+                                 className="d-inline-block"
+                            />
+
+                            <NavDropdown className='user' title="Andrzej Duda">
+                                <NavDropdown.Header>Opcje konta</NavDropdown.Header>
+                                <NavDropdown.Item href="/login">
+                                    Wyloguj się
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
         )
     }
 }
