35bd79d62b103b08230e15d4d1cc7c4c3a39c119
savicci <koziol.grzesiek@interia.pl>
2020-01-11 20:56:05 +0100

    Adding authentication with JWT tokens from another repo.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React from 'react';
-import './App.css';
-import {NavigationBar} from "./components/navbar/NavigationBar";
-import {Layout} from "./components/Layout";
-import {BrowserRouter} from "react-router-dom";
-import MainRoutes from "./routes/MainRoutes";
+import { Router, Route, Link } from 'react-router-dom';
 
-function App() {
-    return (
-        <React.Fragment>
-            <BrowserRouter>
-                <NavigationBar/>
-                    <Layout>
-                        <MainRoutes/>
-                    </Layout>
-            </BrowserRouter>
-        </React.Fragment>
-    );
+import { history } from '../src/new/_helpers';
+import { authenticationService } from '../src/new/_services';
+import { PrivateRoute } from '../src/new/_components';
+import { HomePage } from '../src/new/HomePage';
+import { LoginPage } from '../src/new/LoginPage';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentUser: null
+        };
+    }
+
+    componentDidMount() {
+        authenticationService.currentUser.subscribe(x => this.setState({ currentUser: x }));
+    }
+
+    logout() {
+        authenticationService.logout();
+        history.push('/login');
+    }
+
+    render() {
+        const { currentUser } = this.state;
+        return (
+            <Router history={history}>
+                <div>
+                    {currentUser &&
+                    <nav className="navbar navbar-expand navbar-dark bg-dark">
+                        <div className="navbar-nav">
+                            <Link to="/" className="nav-item nav-link">Home</Link>
+                            <button onClick={this.logout} className="nav-item nav-link">Logout</button>
+                        </div>
+                    </nav>
+                    }
+                    <div className="jumbotron">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-6 offset-md-3">
+                                    <PrivateRoute exact path="/" component={HomePage} />
+                                    <Route path="/login" component={LoginPage} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Router>
+        );
+    }
 }
 
-export default App;
+export { App };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {App} from './App';
+
+// setup fake backend
+import {configureFakeBackend} from '../src/new/_helpers';
+
+configureFakeBackend();
+
+ReactDOM.render(<App/>, document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
